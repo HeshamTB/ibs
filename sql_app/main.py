7dab73133795cccd4e01ff0a5fdb4d53ee81871e
HeshamTB <hishaminv@gmail.com>
2022-04-03 14:55:39 +0300

    sql_app/schema: fix type error
    Signed-off-by: HeshamTB <hishaminv@gmail.com>

--- sql_app/main.py
+++ sql_app/main.py
@@ -50,7 +50,7 @@ def create_item_for_user(
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
-@app.get("/items/", response_model=list[schemas.IotEntity])
+@app.get("/items/", response_model=List[schemas.IotEntity])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
