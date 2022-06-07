311058f09b732ac93288e506cedb6fdb8b893575
HeshamTB <hishaminv@gmail.com>
2022-06-07 14:41:06 +0300

    sql_app: init_secrets: add new variable for first user password
    Signed-off-by: HeshamTB <hishaminv@gmail.com>

--- sql_app/init_secrets.sh
+++ sql_app/init_secrets.sh
@@ -4,3 +4,15 @@ echo "API_KEY=$(./gen_secret.sh)" >> .env
 echo "API_KEY_NAME=big_boy" >> .env
 echo "jwt_secret=$(./gen_secret.sh)" >> .env
 echo "jwt_algorithm=HS256" >> .env
+
+read -s -p "First User password: " firstpass
+echo 
+read -s -p "Retype First User password: " secondpass
+echo
+
+if [ $firstpass != $secondpass ];
+then
+    echo "Passwords dont match!"
+    exit 255
+fi
+echo "first_user_pass=$firstpass" >> .env
